c71f8b53d7cd0a618f223a400b951da735c75fdc
monkalynn813 <byakuya813@hotmail.com>
2019-01-22 21:39:39 -0600

    edit cursor_control

--- cursor_control/src/cursorcontrol.py
+++ cursor_control/src/cursorcontrol.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 import os
+import rospy
+from std_msgs.msg import String
+import numpy as np
 
 
-os.system("xdotool mousemove_relative 10 0")
+def main():
+    rospy.init_node('motor_imagine',anonymous=True)
+    sample_data_sub=rospy.Subsriber('/mindcontrol/channel_data',String,ml_model)
+    os.system("xdotool mousemove_relative 10 0")
+
+def ml_model(data):
+    pass
