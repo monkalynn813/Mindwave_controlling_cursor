95cfaa9b692ecdbbb8cfb888075e709973ee05a7
monkalynn813 <byakuya813@hotmail.com>
2019-02-02 11:44:17 -0600

    editing filter

--- datastreaming/src/samplestream.py
+++ datastreaming/src/samplestream.py
@@ -5,6 +5,7 @@ import rospy
 import numpy as np
 from std_msgs.msg import String
 from scipy import signal
+from matplotlib import pyplot as plt
 
 
 
@@ -17,7 +18,7 @@ class datastreaming:
         rospy.sleep(1.0)
 
         self.fs=250 #record frequency for Cyton board
-        self.frame=250 #once the data matrix is N times this number, process DSP
+        self.frame=1250 #once the data matrix is N times this number, process DSP
         self.channelnum=8 #using 8 channel Cyton biosensing board
         self.raw_data=[]
         self.filtered_data=[]
@@ -31,11 +32,13 @@ class datastreaming:
         self.data_publisher_tdomain=rospy.Publisher('/mindcontrol/filtered_data',String,queue_size=20)
         self.data_publisher_fdomain=rospy.Publisher('/mindcontrol/average_amp',String,queue_size=20)
         self.analysis_time=0
-    def filter(self,sample):
         
+    def filter(self,sample):
 
-        if len(self.raw_data)!= 0 and len(self.raw_data) %self.frame ==0:
-            self.raw_data=self.raw_data[-self.buff:]
+        if len(self.raw_data)>=self.frame:
+            if self.analysis_time==0: print('=======frame initialized, start to analysis========')
+        # if len(self.raw_data)!= 0 and len(self.raw_data) %self.frame ==0:
+            self.raw_data=self.raw_data[-self.frame:]
             _average_amp=[]
             ##Store buff number of data in the matrix for DSP
             channel_extract=np.array(self.raw_data)
@@ -43,35 +46,39 @@ class datastreaming:
                 
                 channel_data=channel_extract[:,k]
                 
-                channel_bp=self.bandpass(self.band[0],self.band[1],channel_data,self.fs) #only pass the bandpass filter
-                channel_filtered=self.notch(self.notch_val,channel_bp,self.fs)#pass bandpass filtered data through notch filter
+                # channel_bp=self.bandpass(self.band[0],self.band[1],channel_data,self.fs) #only pass the bandpass filter
+                # channel_filtered=self.notch(self.notch_val,channel_bp,self.fs)#pass bandpass filtered data through notch filter
                 
                 ##FFT for particular channel##
-                freq,y=self.fft(channel_filtered,self.fs)
-                ##insert figure here?
+                self.freq,self.y=self.fft(channel_data,self.fs)
+                
+                # insert figure here?
                 ##Reduce dimension of FFT data
-                freq_ind=np.where((freq>=self.band[0])&(freq<=self.band[1]))[0] #take diresed frequency
-                desired_freq=freq[freq_ind]
-                amp_of_desired_freq=y[freq_ind]
-                average_amp_desired=np.mean(amp_of_desired_freq)
+                freq_ind=np.where((self.freq>=self.band[0])&(self.freq<=self.band[1]))[0] #take diresed frequency
+                self.desired_freq=self.freq[freq_ind]
+                self.amp_of_desired_freq=self.y[freq_ind]
+                
+                average_amp_desired=np.mean(self.amp_of_desired_freq)
                 
                 _average_amp.append(average_amp_desired)   #put all channel's average amp for particular frequency in matrix             
                 
+
                 #put filtered data in (datanum * channelnum) matrix
-                channel_filtered=channel_filtered.reshape(len(channel_filtered),1) 
-                if k==0:
-                    self.filtered_data=channel_filtered
-                else:
-                    self.filtered_data=np.append(self.filtered_data,channel_filtered,axis=1) #alwasy store the latest filtered data (past 2500 gourps of data)
+                # channel_filtered=channel_filtered.reshape(len(channel_filtered),1) 
+                # if k==0:
+                #     self.filtered_data=channel_filtered
+                # else:
+                #     self.filtered_data=np.append(self.filtered_data,channel_filtered,axis=1) #alwasy store the latest filtered data (past 2500 gourps of data)
             
             self.average_amp=np.append(self.average_amp,np.array([_average_amp]),axis=0) #Matrix of average amp for all channels through out time (for recording purpose)
 
-            filtered_sample = self.filtered_data[-1]
-            filtered_sample=str(filtered_sample)
+            # filtered_sample = self.filtered_data[-1]
+            # filtered_sample=str(filtered_sample)
             average_amp_sample=str(_average_amp)
-            self.data_publisher_tdomain.publish(filtered_sample)
+            # self.data_publisher_tdomain.publish(filtered_sample)
             self.data_publisher_fdomain.publish(average_amp_sample)
             self.analysis_time=self.analysis_time+1
+        
 
         self.raw_data.append(sample.channel_data)
         
@@ -95,10 +102,12 @@ class datastreaming:
 
 
     def stream(self):
-        self.eeg.start_streaming(self.filter,5)
-        print(self.average_amp)
+        self.eeg.start_streaming(self.filter,10)
         print(self.average_amp.shape)
+        print(np.array(self.raw_data).shape)
         print(self.analysis_time)
+        
+
         
 
 def main():
